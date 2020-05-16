df594bec696ab2f1004121e9c58b3c730eb39b6c
xiewx <2623220561@qq.com>
2020-05-16 20:21:46 +0800

    feat(修改配置):新增表单

--- src/pages/addres/index.js
+++ src/pages/addres/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import ReactDOM from "react-dom";
 import 'antd-mobile/dist/antd-mobile.css';
-import { Picker, List, InputItem , Button} from 'antd-mobile'
+import { Picker, List, InputItem , Button, Toast} from 'antd-mobile'
 import { createForm } from 'rc-form'
 import {getAddresData } from './data';
 import { createHashHistory } from 'history'; // 如果是hash路由
+import "./index.css"
 
 const Item = List.Item;
 const history = createHashHistory();
@@ -14,15 +16,49 @@ const CustomChildren = props => (
         style={{ backgroundColor: '#fff', paddingLeft: 15 }}
     >
         <div className="test" style={{ display: 'flex', height: '45px', lineHeight: '45px',position:'relative',borderBottom:0 }}>
-            <div style={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{props.children}</div>
-            <div style={{ textAlign: 'right', color: '#888', marginRight: 15 }}>{props.extra}</div>
+            <div style={{ textAlign: 'left', width: '90px', fontSize: '17px'}}>省市区</div>
+            <div style={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', fontSize: '17px'}}>{props.extra}</div> 
+            {/* children */}
+            {/* <div style={{ textAlign: 'right', color: '#888', marginRight: 15 }}>{props.extra}</div> */}
         </div>
     </div>
 );
 
-function Example() {
+function BasicInputExample(props) {
     const [pickerValue, setPickerValue] = useState("");
 
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [addres, setAddres] = useState("");
+    const [hasError, setHasError] = useState(false);
+    const onChange = (type,value) => {
+        if(type === 'name'){
+            if(value != '' || value != null){
+                setName(value)
+            }
+        }else if(type === 'number'){
+            // debugger
+            // if (!(value.replace(/\s/g, '').length < 11)) {
+            //     setPhoneNumber(value)
+            // }
+            setPhoneNumber(value)
+        }else if(type === 'addres'){
+            if(value != '' || value != null){
+                setAddres(value)
+            }
+        }
+    }
+
+    const onErrorClick = (value) => {
+        // if (hasError) {
+          Toast.info(value);
+        // }
+    }
+
+    const onSubmit = () =>{
+        console.log()
+
+    }
     // window.wx.config({
     //     debug: true,
     //     // appId: dataJson.appid,
@@ -60,48 +96,56 @@ function Example() {
         antdDistrict.push(itemLevel1)
     });
 
-    const { getFieldProps, getFieldError } = this.props.form;
+    const { getFieldProps, getFieldError } = props.form;
 
     return (
         <div>
-            <Picker
-                title="选择地区"
-                extra="请选择地区"
-                data={antdDistrict}
-                value={pickerValue}
-                onChange={v => setPickerValue(v)}
-                onOk={v => setPickerValue(v)}
-                onClick={()=>{console.log('xx')}}
-            >
-                <CustomChildren></CustomChildren>
-            </Picker>
+            
             <form>
                 <List
-                    renderHeader={() => 'Form Validation'}
+                    renderHeader={() => '设置收货地址'}
                     renderFooter={() => getFieldError('account') && getFieldError('account').join(',')}
                 >
+                    <Item>
+                        <Button className={"addresBottom"} type="primary" size="small" inline onClick={onSubmit}>获取微信地址</Button>
+                    </Item>
                     <InputItem
-                    {...getFieldProps('account', {
-                        // initialValue: 'little ant',
-                        rules: [
-                        { required: true, message: 'Please input account' },
-                        { validator: this.validateAccount },
-                        ],
-                    })}
-                    clear
-                    error={!!getFieldError('account')}
-                    onErrorClick={() => {
-                        alert(getFieldError('account').join('、'));
-                    }}
-                    placeholder="please input account"
-                    >Account</InputItem>
-                    <InputItem {...getFieldProps('password')} placeholder="please input password" type="password">
-                    Password
-                    </InputItem>
+                        placeholder="请输入联系人"
+                        onErrorClick={()=>{onErrorClick("请输入联系人")}}
+                        onChange={(e)=>{onChange("name",e)}}
+                        error={(name === '')} 
+                    >联系人</InputItem>
+                    <InputItem
+                        type="phone"
+                        placeholder="请输入手机号码"
+                        error={(phoneNumber.replace(/\s/g, '').length < 11 && phoneNumber.replace(/\s/g, '').length!=0)}
+                        onErrorClick={()=>{onErrorClick("请输入正确的手机号")}}
+                        onChange={(e)=>{onChange("number",e)}}
+                        value={phoneNumber}
+                    >手机号码</InputItem>
+                    <Picker
+                        title="选择地区"
+                        extra="请选择省市区"
+                        data={antdDistrict}
+                        value={pickerValue}
+                        onChange={v => setPickerValue(v)}
+                        onOk={v => setPickerValue(v)}
+                        onClick={()=>{console.log('xx')}}
+                    >
+                        <CustomChildren></CustomChildren>
+                    </Picker>
+                    <InputItem
+                        placeholder="请输入详细地址"
+                        error={!(addres === '')} 
+                        onErrorClick={()=>{onErrorClick("请输入详细地址")}}
+                        onChange={(e)=>{onChange("addres",e)}}
+                        value={addres}
+                    >详细地址</InputItem>
+
+
                     
                     <Item>
-                    <Button type="primary" size="small" inline onClick={this.onSubmit}>Submit</Button>
-                    <Button size="small" inline style={{ marginLeft: '2.5px' }} onClick={this.onReset}>Reset</Button>
+                        <Button className={"addresBottom"} type="primary" size="small" inline onClick={onSubmit}>免押金租借</Button>
                     </Item>
                 </List>
                 </form>
@@ -109,4 +153,8 @@ function Example() {
    );
 }
 
-export default Example
+const BasicInputExampleWrapper = createForm()(BasicInputExample);
+
+// ReactDOM.render(<BasicInputExampleWrapper />, mountNode);
+
+export default BasicInputExampleWrapper
